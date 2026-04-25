dea0b0319d38d9cc52ecdc45201a84d5bd2ee92e
dott13 <supreme.notfound@gmail.com>
2026-04-25 14:54:15 +0300

    refactor: remade auth logic to redirect corectly with deep link(github only for now)

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { routeTree } from "./routeTree.gen";
 import "./App.css";
 import { useAuth } from "./hooks/useAuth";
 import { AuthShell } from "./components/auth/AuthShell";
+import { onOpenUrl } from "@tauri-apps/plugin-deep-link";
+import { useEffect } from "react";
 
 const router = createRouter({ routeTree });
 
@@ -15,6 +17,13 @@ declare module "@tanstack/react-router" {
 
 function App() {
   const { user, loading } = useAuth();
+
+  useEffect(() => {
+  onOpenUrl((urls) => {
+    console.log("deep link received:", urls);
+    alert(JSON.stringify(urls)); // ugly but confirms it works
+  });
+}, []);
 
   if (loading) {
     return <div className="h-screen flex items-center justify-center bg-zinc-950 text-zinc-500">Initializing...</div>;
